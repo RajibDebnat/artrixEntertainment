cee631d62310ab9e48e542eccd7392515c87663a
Rajib Debnath <rajibdebnath923@gmail.com>
2025-03-20 23:55:29 +0530

    added

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "@/components/ui/Card";
+
 
 function page() {
   return (
--- components/Services.tsx
+++ components/Services.tsx
@@ -2,38 +2,50 @@
 import React from "react";
 import { services } from "@/lib/data";
 
-import { SparklesCore } from './animate-components/sparkles'
+import { SparklesCore } from "./animate-components/sparkles";
 import Image from "next/image";
 import logo from "../public/logo.png";
 import { PinContainer } from "./ui/3d-pin";
 function Services() {
   return (
     <section id="services" className="p-10 max-lg:p-0 max-md:p-6 max-sm:p-2">
-       <h2 className="text-6xl my-8 max-md:mb-4  font-bold text-center font-bebas max-md:text-5xl max-sm:text-4xl tracking-wide">
-        Our <Image src={logo} alt="artrix entertainment logo image" height={100} width={100} className=" inline-block rounded-full"/> <span className="  text-primary-yellow"> Services</span>
+      <h2 className="text-6xl my-8 max-md:mb-4  font-bold text-center font-bebas max-md:text-5xl max-sm:text-4xl tracking-wide">
+        Our{" "}
+        <Image
+          src={logo}
+          alt="artrix entertainment logo image"
+          height={100}
+          width={100}
+          className=" inline-block rounded-full"
+        />{" "}
+        <span className="  text-primary-yellow"> Services</span>
       </h2>
       <div className=" w-full flex items-center max-xl:gap-8  max-lg:gap-12  justify-center max-xl:flex-wrap ">
         {services.map((service) => (
           <PinContainer
-          key={service.id}
-          title={service.heading}
-        href={service.link}
-        >
-        <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
-          <h3 className=" font-roboto tracking-widest max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-            {service.heading}
-          </h3>
-          <div className="text-base font-roboto mb-4  !p-0 font-normal">
-            <span className="text-slate-500 ">
-             {service.para}
-            </span>
-          </div>
-          
-        <Image src={service.image} alt='service images ' width={400} height={400} className=" object-fill rounded-xl"/>
-        </div>
-      </PinContainer>
+            key={service.id}
+            title={service.heading}
+            href={service.link}
+          >
+            <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+              <h3 className=" font-roboto tracking-widest max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+                {service.heading}
+              </h3>
+              <div className="text-base font-roboto mb-4  !p-0 font-normal">
+                <span className="text-slate-500 ">{service.para}</span>
+              </div>
+
+              <Image
+                src={service.image}
+                alt="service images "
+                width={400}
+                height={400}
+                className=" object-fill rounded-xl"
+              />
+            </div>
+          </PinContainer>
         ))}
-    </div>
+      </div>
     </section>
   );
 }
