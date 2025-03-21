6cbf3f1c844e5d8c0577f5fd1a7e37a17a09fd52
Rajib Debnath <rajibdebnath923@gmail.com>
2025-03-21 10:49:31 +0530

    added some mistakes

--- components/SuccessStory.tsx
+++ components/SuccessStory.tsx
@@ -3,10 +3,12 @@ import Marquee from "react-fast-marquee";
 import Image from "next/image";
 import { successStory1 ,successStory2} from "@/lib/data";
 import {cn} from "@/lib/utils";
+import logo from "../public/logo.png";
 function SuccessStory() {
   return (
-<section>
-<div className="relative flex h-[50rem] w-full items-center justify-center bg-white dark:bg-black">
+<section className=" p-10 max-lg:p-6 max-md:p-4  max-sm:p-0">
+
+<div className="relative flex  h-full w-full items-center justify-center bg-white dark:bg-black">
       <div
         className={cn(
           "absolute inset-0",
@@ -17,8 +19,10 @@ function SuccessStory() {
       />
       {/* Radial gradient for the container to give a faded look */}
       <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
-
     <div>
+      <h2 className="text-6xl my-8 max-md:mb-4 relative z-40  font-bold text-center font-bebas max-md:text-5xl max-sm:text-4xl tracking-wide">
+        Our <Image src={logo} alt="artrix entertainment logo image" height={100} width={100} className=" inline-block rounded-full"/> <span className="  text-primary-yellow"> Success Stories</span>
+      </h2>
       <Marquee speed={80} className=" gap-10 rounded-xl">
         {successStory1.map((story) => (
           <div key={story.id} className="flex h-80 rounded-xl  mx-8 items-center space-x-4 gap-10">
@@ -36,7 +40,7 @@ function SuccessStory() {
 
       <Marquee direction="right" speed={80} className=" gap-10 rounded-xl">
         {successStory2.map((story) => (
-          <div key={story.id} className="flex h-80 rounded-xl  mx-8 items-center space-x-4 gap-10">
+          <div key={story.id} className="flex h-80 max-md:h-64 max-sm:h-52 rounded-xl  mx-8 items-center space-x-4 gap-10">
             <Image
               src={story.image}
               alt={story.name}
